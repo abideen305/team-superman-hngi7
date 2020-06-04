05853d70248bf9337bce56125a752e6c1beb266d
abideen305 <59992054+abideen305@users.noreply.github.com>
2020-06-04 15:39:16 -0600

    Update AbideenMuh.py

--- scripts/AbideenMuh.py
+++ scripts/AbideenMuh.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 #HNGi7
-#stage 2
-print("Hello World!\nThis is Abideen Muhammed.\nMy HNGi7 ID is HNG-02878\nI am using Python for stage 2 task.")
-#We are to add email to it.
-email = 'abideenmuhammed2018@gmail.com'
-print( email)
+myName = 'Abideen  Muhammed'
+myID = 'HNG-02878'
+lang = 'Python'
+myEmail ='abideenmuhammed2018@gmail.com'
+print("Hello World, this is" +' '+ myName +" "+ "with HNGi7 ID" +" "+ myID +" "+ "using" +' '+ lang +" "+ "for stage 2 task."+""+myEmail)
+    
