0175fa5a358e5c0d7e1e95ce02d9eeb7f6700358
Abideen Muhammed <abideenmuhammed2018@gmail.com>
2020-06-04 08:38:16 -0600

    f

---
+++ muh.py
@@ -0,0 +1,6 @@
+def myself():
+    myName = 'Abideen Muhammed'
+    ID = 'HNG-02878'
+    myEmail ='abideenmuhammed2018@gmail.com'
+    print("Hello World,this is", myName+"." + "My HNGi7 ID is", ID+"." + "I am using Python for stage 2 task"+"."+myEmail)
+myself()
